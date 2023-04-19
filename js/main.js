0834fb84225ec86c0f259c1242748ed25bda6289
John Kinya <kingjohnkn@gmail.com>
2023-04-19 12:26:44 +0300

    update items to cart

--- js/main.js
+++ js/main.js
@@ -67,22 +67,34 @@ function addCartClicked(event) {
 }
 function addProductToCart(title, price, productImg) {
     var cartShopBox = document.createElement("div");
-    // cartShopBox.classList.add("cart-box");
+    cartShopBox.classList.add("cart-box");
     var cartItems = document.getElementsByClassName("cart-content")[0];
     var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
     for (var i = 0; i < cartItemsNames.length; i++) {
-        alert("You have already added this item to cart");
+        if (cartItemsNames[i].innerText == title) {
+            alert("You have already added this item to cart");
+            return;
+        }
     }
+    var cartBoxContent = `
+                            <img src="img/product2.jpg" alt="" class="cart-img">
+                            <div class="detail-box">
+                                <div class="cart-product-title">Earbuds</div>
+                                <div class="cart-price">Ksh. 2500</div>
+                                <input type="number" value="1" class="cart-quantity">
+                            </div>
+                            <!-- Remove Cart -->
+                            <i class='bx bxs-trash-alt cart-remove'></i>`;
+    cartShopBox.innerHTML = cartBoxContent;
+    cartItems.append(cartShopBox);
+    cartShopBox
+        .getElementsByClassName("cart-remove")[0]
+        .addEventListener("click", removeCartItem);
+    cartShopBox
+        .getElementsByClassName("cart-quantity")[0]
+        .addEventListener("change", quantityChanged);
 }
-var cartBoxContent = `
-                        <img src="img/product2.jpg" alt="" class="cart-img">
-                        <div class="detail-box">
-                            <div class="cart-product-title">Earbuds</div>
-                            <div class="cart-price">Ksh. 2500</div>
-                            <input type="number" value="1" class="cart-quantity">
-                        </div>
-                        <!-- Remove Cart -->
-                        <i class='bx bxs-trash-alt cart-remove'></i>`;
+
 // Update Total
 function updateTotal() {
     var cartContent = document.getElementsByClassName("cart-content")[0];
